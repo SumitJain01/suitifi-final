4825d10387b5d3e6b17f9a887709b298c2419b60
SumitJain25 <sumitjaini1998@gmail.com>
2022-01-29 20:19:35 +0530

    Checkout

--- containers/Checkout/components/CheckoutPage/index.js
+++ containers/Checkout/components/CheckoutPage/index.js
@@ -131,7 +131,6 @@ function CheckoutPage({
         </div>
     )
   }
-console.log("Hiiiiii");
   return (
     <Grid
       fluid
@@ -140,7 +139,57 @@ console.log("Hiiiiii");
       {isMobile ? <Divider /> : null}
       <LoadingStateWrapper loading={otpLoading}>
         <CheckoutWrapper  isMobile={isMobile}>
-         hii
+          <Row className="mainRow">
+            <Col xs={12} sm={7} md={7} lg={7}>
+              {/* <LoginSegment
+                isActive={currentUIStep === 0}
+                next={() => setCurrentStep(1)}
+                makeActive={() => setCurrentStep(0)}
+              /> */}
+              <div >
+                <AddressSegment
+                  isActive={currentUIStep === 1}
+                  next={() => setCurrentStep(2)}
+                  makeActive={() => setCurrentStep(1)}
+                  setValid={setIsValid}
+                  setPhone={setPhone}
+                  checkError={checkError}
+                  setCheckError={setCheckError}
+                />
+                {!isMobile && (
+                  <LoadingStateWrapper loading={loading || placeOrderLoading}>
+                    <CustomPaymentSegment
+                      isValid={isValid}
+                      setCheckError={setCheckError}
+                      isNewPaymentUI={isNewPaymentUI}
+                      UAisMobile={isMobile}
+                    />
+                  </LoadingStateWrapper>
+                )}
+              </div>
+            </Col>
+
+            <Col xs={12} sm={5} md={5} lg={5}>
+                <LoadingStateWrapper loading={loading}>
+                  <div className="oldOrderSummaryWrapper">
+                    <Htag>Order Summary</Htag>
+                    <OrderSummary isCheckoutPage />
+                  </div>
+                </LoadingStateWrapper>
+            </Col>
+            {isMobile && (
+              <Col xs={12} sm={12} md={7} lg={7}>
+                  <LoadingStateWrapper loading={loading || placeOrderLoading}>
+                    <CustomPaymentSegment
+                      isValid={isValid}
+                      setCheckError={setCheckError}
+                      isNewPaymentUI={isNewPaymentUI}
+                      UAisMobile={isMobile}
+                    />
+                  </LoadingStateWrapper>       
+              </Col>
+            )}
+          </Row>
         </CheckoutWrapper>
       </LoadingStateWrapper>
     </Grid>
